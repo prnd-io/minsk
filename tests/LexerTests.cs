e2c553a091359b1b9ba2229fc5f1ecf4d1b0d19a
Dominique Goettner <dg@prnd.io>
2018-10-07 02:36:43 +0200

    Added unit tests for the lexer.

---
+++ tests/LexerTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Xunit;
+using Minsk.CodeAnalysis;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Tests
+{
+    public class LexerTests
+    {
+        [Fact]
+        public void Lexer_Should_Tokenize_Number()
+        {
+            var lexer = new Lexer("123");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.NumberToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("123");
+                token.Value.Should().BeOfType(typeof(System.Int32));
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_Whitespace()
+        {
+            var lexer = new Lexer(" ");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.WhitespaceToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be(" ");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_Plus()
+        {
+            var lexer = new Lexer("+");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.PlusToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("+");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_Minus()
+        {
+            var lexer = new Lexer("-");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.MinusToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("-");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_Star()
+        {
+            var lexer = new Lexer("*");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.StarToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("*");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_Slash()
+        {
+            var lexer = new Lexer("/");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.SlashToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("/");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_OpenParenthesis()
+        {
+            var lexer = new Lexer("(");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.OpenParenthesisToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("(");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_CloseParenthesis()
+        {
+            var lexer = new Lexer(")");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.CloseParenthesisToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be(")");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_EndOfFile()
+        {
+            var lexer = new Lexer("");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.EndOfFileToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("\0");
+                token.Value.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Lexer_Should_Tokenize_BadToken()
+        {
+            var lexer = new Lexer("@");
+            var token = lexer.NextToken();
+
+            using (new AssertionScope())
+            {
+                token.Kind.Should().Be(SyntaxKind.BadToken);
+                token.Position.Should().Be(0);
+                token.Text.Should().Be("@");
+                token.Value.Should().BeNull();
+            }
+        }
+    }
+}
